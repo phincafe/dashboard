da5f39ecdc0851302c46a0b11c8654bce442fc32
phincafe <jason@phin.cafe>
2025-12-04 14:40:41 -0800

    Update main.jsx

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -2,9 +2,12 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App'
 import './index.css'
+import PasswordGate from "./components/PasswordGate.jsx";
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
+    <PasswordGate>
     <App />
+    </PasswordGate>
   </React.StrictMode>,
 )
